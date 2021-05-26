1e0e1c530f36b0f40277e4f0e7ca61fdcc0d5de0
“fanyihua0309” <nuaaccstfyh@163.com>
2021-05-26 17:09:31 +0800

    init: 创建 React 项目，配置 Router/less/antd

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import './App.less';
+import { Button } from 'antd';
+
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
+
+export default App;
